31c85ff25e26b01cf50857134abb05f87cd775e1
SUNGJUN PARK <psg1478795@naver.com>
2023-10-05 16:58:43 +0900

    #1.5: TweenAnimationBuilder(10.05)

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -34,59 +34,22 @@ class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedAlign(
-              alignment: _visibleA ? Alignment.center : Alignment.centerRight,
+            TweenAnimationBuilder(
+              tween: ColorTween(
+                begin: Colors.pink,
+                end: Colors.cyan,
+              ),
+              curve: Curves.bounceInOut,
               duration: const Duration(
-                milliseconds: 500,
+                milliseconds: 1500,
               ),
-              child: AnimatedOpacity(
-                opacity: _visibleA ? 1 : 0,
-                duration: const Duration(
-                  milliseconds: 500,
-                ),
-                child: SizedBox(
-                  width: size.width * 0.4,
-                  height: size.width * 0.4,
-                  child: Container(
-                    color: Colors.amber,
-                    child: const Text(
-                      'Animated Align & Opacity',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              onPressed: _triggerA,
-              child: const Text('Go!'),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            AnimatedContainer(
-              duration: const Duration(
-                milliseconds: 800,
-              ),
-              height: size.width * 0.4,
-              width: size.width * 0.4,
-              transformAlignment: Alignment.center,
-              transform: Matrix4.rotationZ(
-                _visibleB ? 0 : 1,
-              ),
-              decoration: BoxDecoration(
-                color: _visibleB ? Colors.red : Colors.blue,
-                borderRadius: BorderRadius.circular(
-                  _visibleB ? 0 : 100,
-                ),
-              ),
-              child: const Text(
-                'Animated Container',
-                textAlign: TextAlign.center,
-              ),
+              builder: (context, value, child) {
+                return Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/4/4f/Dash%2C_the_mascot_of_the_Dart_programming_language.png',
+                  color: value,
+                  colorBlendMode: BlendMode.colorBurn,
+                );
+              },
             ),
             const SizedBox(
               height: 10,
