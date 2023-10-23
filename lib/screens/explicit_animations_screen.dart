b47bc2c4b0b59fb5a673349cb6fb06c8ab38cd99
SUNGJUN PARK <psg1478795@naver.com>
2023-10-23 16:14:18 +0900

    #2.5: Explicit Widgets(10.23)

--- lib/screens/explicit_animations_screen.dart
+++ lib/screens/explicit_animations_screen.dart
@@ -10,10 +10,34 @@ class ExplicitAnimationScreen extends StatefulWidget {
 
 class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
     with SingleTickerProviderStateMixin {
-  late final Animation<Color?> _color = ColorTween(
-    begin: Colors.amber,
-    end: Colors.red,
+  late final Animation<Decoration> _decoration = DecorationTween(
+    begin: BoxDecoration(
+      color: Colors.red,
+      borderRadius: BorderRadius.circular(
+        30,
+      ),
+    ),
+    end: BoxDecoration(
+      color: Colors.blue,
+      borderRadius: BorderRadius.circular(100),
+    ),
   ).animate(_animationController);
+
+  late final Animation<double> _rotation = Tween(
+    begin: 0.0,
+    end: 2.0,
+  ).animate(_animationController);
+
+  late final Animation<double> _size = Tween(
+    begin: 1.0,
+    end: 0.5,
+  ).animate(_animationController);
+
+  late final Animation<Offset> _slide = Tween(
+    begin: Offset.zero,
+    end: const Offset(-0.1, 0),
+  ).animate(_animationController);
+
   late final AnimationController _animationController = AnimationController(
     vsync: this,
     duration: const Duration(seconds: 2),
@@ -32,6 +56,12 @@ class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
   }
 
   @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -41,19 +71,21 @@ class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedBuilder(
-              animation: _color,
-              builder: (context, child) {
-                return Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: MediaQuery.of(context).size.width * 0.8,
-                  decoration: BoxDecoration(
-                    color: _color.value,
-                    borderRadius:
-                        BorderRadius.circular(_animationController.value * 100),
+            SlideTransition(
+              position: _slide,
+              child: ScaleTransition(
+                scale: _size,
+                child: RotationTransition(
+                  turns: _rotation,
+                  child: DecoratedBoxTransition(
+                    decoration: _decoration,
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.width * 0.8,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                    ),
                   ),
-                );
-              },
+                ),
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
